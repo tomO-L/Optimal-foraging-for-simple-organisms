e88faeb9524eb2b301da8cbf0f16b70fe70221f5
tomO-L <torjollet@gmail.com>
2026-09-05 18:48:09 +0200

    added 4th environment (the random one)

--- fig_insets/insets_visu.py
+++ fig_insets/insets_visu.py
@@ -2,10 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import dill
 
-
-rho = np.load("../rho.npy")
+rho = np.load("../rho_0.npy")
 rho_1 = np.load("../rho_1.npy")
 rho_2 = np.load("../rho_2.npy")
+rho_3 = np.load("../rho_3.npy")
+
 
 # with open("../environment_test_1.pkl", 'rb') as fileopen:
 #     rho_1 = dill.load(fileopen) * 10
@@ -18,7 +19,7 @@ for i in range(3):
     rho = np.concatenate((rho,rho))
     rho_1 = np.concatenate((rho_1[:-200],rho_1))
     rho_2 = np.concatenate((rho_2[:-400],rho_2))
-
+    rho_3 = np.concatenate((rho_3[:7000],rho_3))
 
 ### PLOTS ###
 
@@ -35,7 +36,7 @@ factor_inset = 2
 image_width = 1200
 image_height = 100 #388
 
-color_rho = [(102/255,166/255,30/255), (230/255,171/255,2/255), (166/255,118/255,29/255) ]
+color_rho = [(102/255,166/255,30/255), (230/255,171/255,2/255), (166/255,118/255,29/255), 'red' ]
 color_v = (117/255,112/255,179/255)
 color_tau = (231/255,41/255,138/255)
 color_eta = (27/255,158/255,119/255)
@@ -95,6 +96,25 @@ plt.subplots_adjust(top=top, bottom=bottom, right=right , left=left)
 fig,axes = plt.subplots(1,1, figsize=(image_width/my_dpi, image_height/my_dpi), dpi=my_dpi, sharex=True)
 
 axes.fill_between(np.linspace(0,len(rho_2)-1,len(rho_2)), rho_2, color = color_rho[2], step="mid")
+
+#axes.set_ylim(0,1.1 * max(rho))
+#axes.set_xlim(40,80)
+
+axes.set_xlabel(r'Position ($S$)',fontsize=axes_font_size)
+axes.set_ylabel(r'Density ($\rho$)',fontsize=axes_font_size)
+
+for label in (axes.get_xticklabels() + axes.get_yticklabels()):
+    label.set_fontsize(graduation_font_size)
+
+axes.set_axis_off()
+
+plt.subplots_adjust(top=top, bottom=bottom, right=right , left=left)
+
+# 4
+
+fig,axes = plt.subplots(1,1, figsize=(image_width/my_dpi, image_height/my_dpi), dpi=my_dpi, sharex=True)
+
+axes.fill_between(np.linspace(0,len(rho_3)-1,len(rho_3)), rho_3, color = color_rho[3], step="mid")
 
 #axes.set_ylim(0,1.1 * max(rho))
 #axes.set_xlim(40,80)
